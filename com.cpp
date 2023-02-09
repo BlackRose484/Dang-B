1493d3228ecfe3b5154517de3554b99c4e1da3b8
hung <hungnbc2@gmai.com>
2023-02-09 08:05:39 +0700

    b3

---
+++ com.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a == b && b == c)
+    {
+        cout << "true" << endl;
+    }
+    else
+        cout << "false" << endl;
+    system("pause");
+}
