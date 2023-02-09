3c30b7340d762132d135479150fe9f63acfd5a2f
hung <hungnbc2@gmai.com>
2023-02-09 08:08:19 +0700

    b4

---
+++ Divisibility.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a % 7 == 0 && b % 7 == 0)
+    {
+        cout << "true\n";
+    }
+    else
+        cout << "false\n";
+    system("pause");
+}
